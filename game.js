9c03dfb02af39bb327f82242923bb0b115fe8dce
AliHakim773 <alihakim7732@gmail.com>
2023-11-12 14:14:05 +0200

    added startSequence and pushNextSequence functions

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // variables
 let game_on = false
 let level = 1
+const delay = 500
 const game_sequence = []
 
 const level_title = document.getElementById("level-title")
@@ -18,27 +19,39 @@ const blue_audio = new Audio("./sounds/blue.mp3")
 
 const startGame = () => {
     game_on = true
+    titleRefresh()
     pushNextSequence()
+    startSequence()
 }
 const endGame = () => {
     game_on = false
     level = 1
 }
+const playGreenAudio = () => green_audio.play()
+const playRedAudio = () => red_audio.play()
+const playYellowAudio = () => yellow_audio.play()
+const playBlueAudio = () => blue_audio.play()
+const playAudio = (i) => {
+    if (i == 0) playGreenAudio()
+    else if (i == 1) playRedAudio()
+    else if (i == 2) playYellowAudio()
+    else if (i == 3) playBlueAudio()
+}
 const onGreenClick = () => {
     if (!game_on) return
-    green_audio.play()
+    playGreenAudio()
 }
 const onRedClick = () => {
     if (!game_on) return
-    red_audio.play()
+    playRedAudio()
 }
 const onYellowClick = () => {
     if (!game_on) return
-    yellow_audio.play()
+    playYellowAudio()
 }
 const onBlueClick = () => {
     if (!game_on) return
-    blue_audio.play()
+    playBlueAudio()
 }
 const titleRefresh = () => {
     if (game_on) {
@@ -46,11 +59,14 @@ const titleRefresh = () => {
     } else level_title.innerHTML = "Game Over, Press Any Key To Restart"
 }
 const pushNextSequence = () => game_sequence.push(Math.floor(Math.random() * 4))
-
+const startSequence = () => {
+    for (const i of game_sequence) {
+        setTimeout(playAudio(i), i * 100)
+    }
+}
 document.getElementsByTagName("body")[0].addEventListener("keypress", () => {
     if (!game_on) {
         startGame()
-        titleRefresh()
     }
 })
 green.addEventListener("click", onGreenClick)
