ef87e4f60b669ab9c8460cf2c9a2c4802e16e8d6
AliHakim773 <alihakim7732@gmail.com>
2023-11-12 13:35:21 +0200

    added audio to buttons click

--- game.js
+++ game.js
@@ -1,30 +1,49 @@
 // variables
 let game_on = false
+let level = 1
 
+const level_title = document.getElementById("level-title")
 const green = document.getElementById("green")
 const red = document.getElementById("red")
 const yellow = document.getElementById("yellow")
 const blue = document.getElementById("blue")
 
+const wrong_audio = new Audio("./sounds/wrong.mp3")
+const green_audio = new Audio("./sounds/green.mp3")
+const yellow_audio = new Audio("./sounds/yellow.mp3")
+const red_audio = new Audio("./sounds/red.mp3")
+const blue_audio = new Audio("./sounds/blue.mp3")
+
 const startGame = () => (game_on = true)
-const endGame = () => (game_on = false)
+const endGame = () => {
+    game_on = false
+    level = 1
+}
 
 const onGreenClick = () => {
     if (!game_on) return
+    green_audio.play()
 }
 const onRedClick = () => {
     if (!game_on) return
+    red_audio.play()
 }
 const onYellowClick = () => {
     if (!game_on) return
+    yellow_audio.play()
 }
 const onBlueClick = () => {
     if (!game_on) return
+    blue_audio.play()
+}
+const titleRefresh = () => {
+    if (game_on) {
+        level_title.innerHTML = "Level " + level
+    } else level_title.innerHTML = "Game Over, Press Any Key To Restart"
 }
 
 document.getElementsByTagName("body")[0].addEventListener("keypress", () => {
     if (!game_on) {
-        console.log("Hi")
         startGame()
     }
 })
