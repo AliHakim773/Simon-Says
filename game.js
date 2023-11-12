bd152b69062fc5c4c0c4502af7e527c391962999
AliHakim773 <alihakim7732@gmail.com>
2023-11-12 13:19:20 +0200

    added onClick to the buttons

--- game.js
+++ game.js
@@ -1,16 +1,34 @@
 // variables
 let game_on = false
 
+const green = document.getElementById("green")
+const red = document.getElementById("red")
+const yellow = document.getElementById("yellow")
+const blue = document.getElementById("blue")
+
+const startGame = () => (game_on = true)
+const endGame = () => (game_on = false)
+
+const onGreenClick = () => {
+    if (!game_on) return
+}
+const onRedClick = () => {
+    if (!game_on) return
+}
+const onYellowClick = () => {
+    if (!game_on) return
+}
+const onBlueClick = () => {
+    if (!game_on) return
+}
+
 document.getElementsByTagName("body")[0].addEventListener("keypress", () => {
     if (!game_on) {
         console.log("Hi")
         startGame()
     }
 })
-
-const startGame = () => {
-    game_on = true
-}
-const endGame = () => {
-    game_on = false
-}
+green.addEventListener("click", onGreenClick)
+red.addEventListener("click", onRedClick)
+yellow.addEventListener("click", onYellowClick)
+blue.addEventListener("click", onBlueClick)
